0716d914e80b7670d32e5fc874d6bc3f94d40b56
nicolasdraperi <nicolas.draperi2@gmail.com>
2024-02-29 17:39:03 +0100

    manual merge

---
+++ analysis.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+import mysql.connector
+from mysql.connector import Error
+from itertools import islice
+from urllib.parse import urlparse, urljoin
+
+
+
+
+
